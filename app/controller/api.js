24700e9d36937a93276354ca4f9699008a734787
Edward Chan <chan@sikho.ca>
2009-08-18 21:29:52 +0900

    initial commit

---
+++ app/controller/api.js
@@ -0,0 +1,30 @@
+(function() {
+	function secure(fn) {
+		if(request.authorization == "esh:" + config.sitepass) return fn()
+		else return ["unauthorized"]
+	}
+	
+	function create() {
+		return secure(function() {
+			with(eval(request.content)) {
+				if(title != undefined && photo != undefined) {
+					var path = "/tmp/" + Math.floor(Math.random() * 100000) + "." + ext
+					log.info("api create: " + title + " => " + path)
+										
+					open(path, "base64").write(photo)
+					var post = require("model/post.js")(db).persist(null, title, path, tags)
+					if(twit) require("twitter.js")(post)
+					
+					return ["ok", "ok"]
+				} else {
+					return ["ok", "missing title or photo"]
+				}
+			}
+		})
+	}
+	
+	return {
+		create: create
+	}
+})
+
