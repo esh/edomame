72ea7cdad717f6522acd6f0ba22bed02d1da6ad1
Edward Chan <chan@sikho.ca>
2009-08-19 22:24:26 +0900

    forgot a bracket

--- app/controller/admin.js
+++ app/controller/admin.js
@@ -4,7 +4,7 @@
 	}
 
 	function deploy() {
-		if(request.hostname.match(/(github.com)|(engineyard.com)/) {
+		if(request.hostname.match(/(github.com)|(engineyard.com)/)) {
 			log.info("redeploying...")
 			try { log.info(shell("./scripts/deploy.local.sh")) } catch(e) {}
 			return ["ok", ""]	
