814b1655f9fbe1fe3de8f3f8d2779acb51b2e176
Edward Chan <chan@sikho.ca>
2009-08-19 21:22:55 +0900

    updated the script

--- scripts/deploy.local.sh
+++ scripts/deploy.local.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+cd ../../staging/edomame
+git pull
 rsync	--verbose --progress --stats --compress \
 	--recursive --times --perms --links --delete \
 	--exclude "app/config.js" --exclude "public/blog/*" \
