af24d5b19412e5356a7d742f3b4ab3f77cb71e30
Edward Chan <chan@sikho.ca>
2009-08-19 21:24:08 +0900

    deploy scripts too

--- scripts/deploy.local.sh
+++ scripts/deploy.local.sh
@@ -4,6 +4,6 @@ git pull
 rsync	--verbose --progress --stats --compress \
 	--recursive --times --perms --links --delete \
 	--exclude "app/config.js" --exclude "public/blog/*" \
-	--exclude "scripts" --exclude "db" --exclude "LICENSE" \
+	--exclude "db" --exclude "LICENSE" \
 	--exclude "README.markdown" \
 	* ../../live/edomame
