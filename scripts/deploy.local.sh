dd6ccedaa0eb1a5900ced135b28e0c628ac2fd30
Edward Chan <chan@sikho.ca>
2009-09-07 23:24:50 +0900

    added check for deployment

--- scripts/deploy.local.sh
+++ scripts/deploy.local.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 cd ../../staging/edomame
 git pull
+
+if ./latte.sh app:tests deployment_runner.js | grep 'all tests passed'; then
 rsync	--verbose --progress --stats --compress \
 	--recursive --times --perms --links --delete \
 	--exclude "app/config.js" --exclude "public/blog/*" \
-	--exclude "db" --exclude "LICENSE" \
+	--exclude "db/*" --exclude "LICENSE" \
 	--exclude "README.markdown" \
 	* ../../live/edomame
+fi
