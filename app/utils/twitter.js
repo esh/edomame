ac93c47b8be62db89346fd65ca349cbf50ec46d7
Edward Chan <chan@sikho.ca>
2009-09-08 23:54:01 +0900

    reliable twitter

--- app/utils/twitter.js
+++ app/utils/twitter.js
@@ -1,29 +1,28 @@
 require("utils/common.js")
 
 function notify_twitter(model, tweet) {
-	var auth = config.twitteruser + ":" + config.twitterpass
-	auth = "Basic " + auth.toBase64()
+	function reliable(url, params, msg) {
+		// retry every minute for 1 hour until success or failure
+		for(var i = 0 ; i < 60 ; i++) {	
+			try {
+				var h = hopen(url, params)
+				h.write(msg)
+				return h.read()
+			} catch(e) {
+				log.error(e)
+				sleep(1000)
+			}
+		}
+	}
+
+	var auth = "Basic " + (config.twitteruser + ":" + config.twitterpass).toBase64()
 
 	if(tweet) {	
-		var h = hopen("http://twitter.com/statuses/update.json", {"Authorization": auth})
-	
 		var status = "http://edomame.com/" + model.key + " - " + model.title
 		if(status.length > 140) status.substring(0, 137) + "..."
 
-		try {	
-			h.write("status=" + status.escapeURL())
-			log.debug(h.read())
-		} catch(e) {
-			log.error(e)
-		}
+		log.debug(reliable("http://twitter.com/statuses/update.json", {"Authorization": auth}, "status=" + status.escapeURL()))
 	}
 	
-	var h = hopen("http://twitter.com/direct_messages/new.json", {"Authorization": auth})
-
-	try {	
-		h.write("user=listous&text=" + "http://edomame.com/" + model.key) 
-		log.debug(h.read())
-	} catch(e) {
-		log.error(e)
-	}
+	log.debug(reliable("http://twitter.com/direct_messages/new.json", {"Authorization": auth}, "user=listous&text=" + "http://edomame.com/" + model.key)) 
 }
