38d0791fddc435ee078bad7a06ae36719abd9de3
Edward Chan <chan@sikho.ca>
2009-09-10 01:35:36 +0900

    put reliable twitter in a seperate thread

--- app/utils/twitter.js
+++ app/utils/twitter.js
@@ -2,15 +2,18 @@ require("utils/common.js")
 
 function notify_twitter(model, tweet) {
 	function reliable(url, params, msg) {
-		// retry every minute for 1 hour until success or failure
-		for(var i = 0 ; i < 60 ; i++) {	
-			try {
-				return hpost(url, params, msg)
-			} catch(e) {
-				log.error(e)
-				sleep(1000*60)
+		thread(function() {
+			// retry every minute for 1 hour until success or failure
+			for(var i = 0 ; i < 60 ; i++) {	
+				try {
+					log.info(hpost(url, params, msg))
+					return
+				} catch(e) {
+					log.error(e)
+					sleep(1000*60)
+				}
 			}
-		}
+		})
 	}
 
 	var auth = config.twitteruser + ":" + config.twitterpass
@@ -20,8 +23,8 @@ function notify_twitter(model, tweet) {
 		var status = "http://edomame.com/" + model.key + " - " + model.title
 		if(status.length > 140) status.substring(0, 137) + "..."
 
-		log.debug(reliable("http://twitter.com/statuses/update.json", {"Authorization": auth}, "status=" + status.escapeURL()))
+		reliable("http://twitter.com/statuses/update.json", {"Authorization": auth}, "status=" + status.escapeURL())
 	}
 	
-	log.debug(reliable("http://twitter.com/direct_messages/new.json", {"Authorization": auth}, "user=listous&text=" + "http://edomame.com/" + model.key)) 
+	reliable("http://twitter.com/direct_messages/new.json", {"Authorization": auth}, "user=listous&text=" + "http://edomame.com/" + model.key) 
 }
