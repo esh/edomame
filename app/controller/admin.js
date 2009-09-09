876ff7a06c472b52bfded26ffaee74a2f48114ca
Edward Chan <chan@sikho.ca>
2009-09-10 01:24:09 +0900

    added primitive throttling for admin

--- app/controller/admin.js
+++ app/controller/admin.js
@@ -6,8 +6,15 @@
 	function login() {
 		if(request.params["passcode"] == config.sitepass) {
 			session["authorized"] = true
+			session["attempt"] = null
 			return ["redirect", "/"]
-		} else return ["ok", render("view/admin/password.jhtml", new Object())]
+		} else if(session["attempt"] == null || session["last"] < (new Date()).getTime()) {
+			session["attempt"] = session["attempt"] == null ? 1 : session["attempt"] + 1
+			session["last"] = (new Date()).getTime() + 8000 * session["attempt"]
+			return ["ok", render("view/admin/password.jhtml", new Object())]
+		} else {
+			return ["unauthorized"]
+		}
 	}
 	
 	function logout() {
