6324b2291b6dc1258fab0a8f53ee7a2b1347cfd6
Edward Chan <esh@mini-esh.(none)>
2010-03-04 20:52:02 +0900

    split photos across the datatable

--- app/controller/api.js
+++ app/controller/api.js
@@ -6,23 +6,19 @@
 
 	function create() {
 		return secure(function() {
-			with(eval(request.content)) {
-				if(title != undefined && photo != undefined) {
-					var post = require("model/post.js")(ds).persist(
-						null,
-						title,
-						tags,
-						org.apache.commons.codec.binary.Base64.decodeBase64(photo))
+			var t = eval(request.content)
 
-					if(post.tags.indexOf("tweet") != -1) {					
-						require("utils/twitter.js")
-						notify_twitter(post)
-					}
-	
-					return ["ok", "ok"]
-				} else {
-					return ["ok", "missing title or photo"]
+			if(t.title != undefined && t.photo != undefined) {
+				var post = require("model/post.js")(ds).persist(null, t.title, t.tags, t.photo)
+
+				if(post.tags.indexOf("tweet") != -1) {					
+					require("utils/twitter.js")
+					notify_twitter(post)
 				}
+
+				return ["ok", "ok"]
+			} else {
+				return ["ok", "missing title or photo"]
 			}
 		})
 	}
--- app/model/post.js
+++ app/model/post.js
@@ -10,37 +10,31 @@
 	function persist(key, title, tags, photo) {
 		var model
 		ds.transaction(function(ds) {
-			var original = new Array()
-			var preview = new Array()
-			var thumb = new Array()
-			if(photo) {
-				// save the original by splitting into MAX_SIZE byte chunks
-				for(var chunk = 0 ; chunk < Math.ceil(photo.length / MAX_SIZE) ; chunk++) {
-					var b = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, Math.min(photo.length - chunk * MAX_SIZE, MAX_SIZE))
-					for(var i = chunk * MAX_SIZE, j = 0 ; i < Math.min((chunk + 1) * MAX_SIZE, photo.length) ; i++, j++) {
-						b[j] = photo[i]
-					}	
-					original.push(ds.put("original", b))	
-				}
-			}
-
 			// split the tags into an array and ensure we have the "all" tag
 			tags = tags != null && tags.trim().length > 0 ? tags.trim().toLowerCase().split(" ") : new Array()
 			if(tags.indexOf("all") == -1) tags.push("all")
+			
 			model = {
 				title: title,
 				tags: tags,
-				timestamp: new Date(),
-				original: original,
-				preview: preview,
-				thumb: thumb
+				timestamp: new Date()
 			}
 	
 			if(key == null || key == undefined) {
 				key = ds.put("posts", model)
 				model.key = key
 				log.info("new model: " + key)
-			} 
+			}
+
+			ds.root(key)
+
+			if(photo) {
+				model.original = new Array()
+				// save the original by splitting into MAX_SIZE byte chunks
+				for(var chunk = 0 ; chunk < Math.ceil(photo.length / MAX_SIZE) ; chunk++) {
+					model.original.push(ds.put("original", photo.slice(chunk * MAX_SIZE, Math.min((chunk + 1) * MAX_SIZE, photo.length))))
+				}
+			}
 
 			ds.update(key, model)
 
@@ -52,7 +46,14 @@
 	}
 	
 	function remove(key) {
-		ds.remove(key)
+		ds.transaction(function(ds) {
+			ds.root(ds)
+			var model = ds.get(key)
+			model.original.forEach(function(e) {
+				ds.remove(e)
+			})
+			ds.remove(key)
+		})
 	}
 	
 	return {
