da268a4d6b7236eba99fd688e24cbf4d4d7e3b68
Edward Chan <esh@mini-esh.(none)>
2010-03-07 22:38:05 +0900

    added route

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,11 @@
 (function(url) {
-	return url
+        if(url.match(/^\/blog/) || url.match(/^\/admin/) || url.match(/^\/api/) || url.match(/^\/rss/)) return url
+
+        if(url.match(/^\/[a-zA-Z0-9]+$/)) return "/blog/show/all"
+
+        var res = url.match(/^(\/[a-zA-Z]+)(\/[a-zA-Z0-9]+)?$/)
+        if(res != null && res[1] != null) return "/blog/show" + res[1]
+
+        return url
 })
+
