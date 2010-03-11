fdb5eaa4e3c985b7e44834fb92f5bd5aff9e7e65
Edward Chan <esh@mini-esh.(none)>
2010-03-11 20:14:02 +0900

    fix routes

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,7 @@
 	var res
         if(url.match(/^\/blog/) || url.match(/^\/admin/) || url.match(/^\/api/) || url.match(/^\/rss/)) {
 		return url
-	} else if(url.match(/^\/[a-zA-Z0-9]+$/)) {
+	} else if(url.match(/^\/[a-zA-Z]+[0-9]+[a-zA-Z0-9]+$/)) {
 		return "/blog/show/all"	
 	} else if(res = url.match(/^\/([a-zA-Z]+)(\/[a-zA-Z0-9]+)?$/)) {
 		return "/blog/show/" + res[1]
