16b4f4ec2413d1d6e9513029d7a0badc73c4aaf9
Edward Chan <esh@mini-esh.(none)>
2010-04-27 23:04:37 +0900

    fix

--- app/utils/twitter.js
+++ app/utils/twitter.js
@@ -4,7 +4,7 @@ function notify_twitter(model) {
 	var auth = config.twitteruser + ":" + config.twitterpass
 	auth = "Basic " + auth.toBase64()
 
-	var status = "http://edomame.com/" + model.key + " - " + model.title
+	var status = "http://www.edomame.com/" + model.key + " - " + model.title
 	if(status.length > 140) status.substring(0, 137) + "..."
 
 	hpost("http://twitter.com/statuses/update.json", {"Authorization": auth}, "status=" + status.escapeURL())
