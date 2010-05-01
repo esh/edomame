2a0b88065a078440407aed85c9a012850e87c439
Edward Chan <esh@mini-esh.(none)>
2010-05-01 18:58:06 +0900

    parse multi part

--- app/controller/_ah.js
+++ app/controller/_ah.js
@@ -1,7 +1,30 @@
 (function() {
+	require("utils/common.js")
+	importPackage(org.apache.commons.io)
+
 	return {
 		mail: function(address) {
-			log.info("I got mail")
+			var mail = request.mail
+			mail.getFrom().forEach(function(from) {
+				log.info("FROM: " + from)
+			})
+
+			var title = mail.getSubject().trim()	
+			var photo
+			var tags
+
+			var multipart = mail.getContent();
+			for(var i = 0 ; i < multipart.getCount() ; i++) {
+				var part = multipart.getBodyPart(i)
+				log.info("PART TYPE:" + part.getContentType())
+				if(part.getContentType().matches(/text/)) {
+					tags = part.getContent().trim().split(" ")
+				} else if(part.getContentType().matches(/image/)) {
+					photo = IOUtils.toByteArray(part.getInputStream())
+				}	
+			}
+
+			log.info(title + " " + photo + " " + tags)
 
 			return ["ok", "ok"]
 		}
